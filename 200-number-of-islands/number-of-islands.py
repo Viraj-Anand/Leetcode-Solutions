bae4c9001dd4a3e4251f3448bdab1122f230e331
Viraj Anand <106916757+Viraj-Anand@users.noreply.github.com>
2023-09-25 13:40:55 +0530

    Time: 254 ms (96.98%) | Memory: 18.9 MB (75.27%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        def bfs(i,j,grid):
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j]=='0':
+                return 
+
+            grid[i][j]='0'
+            bfs(i+1,j,grid)
+            bfs(i-1,j,grid)
+            bfs(i,j+1,grid)
+            bfs(i,j-1,grid)
+
+
+
+        m=len(grid)
+        n=len(grid[0])
+        count=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=='1':
+                    count+=1
+                    bfs(i,j,grid)
+
+        return count
+        
